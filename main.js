218ec473b3a348667b0791351c6e64b17d1df6a3
Atef Ben Ali <atefBB@users.noreply.github.com>
2022-06-18 12:38:18 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ blog({
   title: "Atef Ben Ali",
   author: "Atef Ben Ali",
   avatar: "https://en.gravatar.com/userimage/83620564/faa64a4b11985e33d76de4144a5319b5.jpeg",
-  avatarClass: "full",
+  avatarClass: "rounded-full",
   links: [
     { title: "Email", url: "mailto:atef.bettaib@gmail.com" },
     { title: "GitHub", url: "https://github.com/atefBB" },
